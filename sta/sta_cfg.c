d28cae0b1a5340b084d15e693365277758612416
chenhaiq <chenhaiq@yunify.com>
2015-07-18 17:44:17 +0800

    fix compiler error on recent kernel

--- sta/sta_cfg.c
+++ sta/sta_cfg.c
@@ -5398,7 +5398,7 @@ VOID RTMPIoctlShow(
             wrq->u.data.length = strlen(extra) + 1; /* 1: size of '\0' */
             break;
         case SHOW_DRVIER_VERION:
-            snprintf(extra, size, "Driver version-%s, %s %s\n", STA_DRIVER_VERSION, __DATE__, __TIME__ );
+            snprintf(extra, size, "Driver version-%s\n", STA_DRIVER_VERSION);
             wrq->u.data.length = strlen(extra) + 1; /* 1: size of '\0' */
             break;
 #ifdef DOT11_N_SUPPORT
